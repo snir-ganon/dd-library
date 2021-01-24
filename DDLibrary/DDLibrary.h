1cfccc61935fd8accc40f9eee605bbe119adc68a
Snir Ganon <snir@lightstrap.com>
2021-01-24 21:47:40 +0200

    Initial Commit

---
+++ DDLibrary/DDLibrary.h
@@ -0,0 +1,18 @@
+//
+//  DDLibrary.h
+//  DDLibrary
+//
+//  Created by Snir Ganon on 24/01/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DDLibrary.
+FOUNDATION_EXPORT double DDLibraryVersionNumber;
+
+//! Project version string for DDLibrary.
+FOUNDATION_EXPORT const unsigned char DDLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DDLibrary/PublicHeader.h>
+
+
